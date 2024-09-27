2f0c7a7f619e1365f85527cdb4a9e913ab4ed04e
Giser <65968233+newpeople123@users.noreply.github.com>
2024-09-27 10:49:59 +0800

    修改build.bat脚本使项目编译更简单;修改.gitignore文件;修改vcpkg.json文件的name;

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
-cd build
-cmake .. -DVCPKG_ROOT=E:\SDK -DENABLE_MATHLIB1=OFF
+@echo off
+if not exist "build" (
+    mkdir build
+)
+cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=E:/SDK/vcpkg/scripts/buildsystems/vcpkg.cmake -S . -B build
+cmake --build build --config Release
+
